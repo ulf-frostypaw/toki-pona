891053f4cca02dd97cf035525eeae2a82dedc22b
Ulf FrostyPaw <19324157@uagro.mx>
2023-04-27 20:59:37 -0600

    v0.1

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} 
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+*/
+
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.tsx',
+})
+ 
+module.exports = withNextra()
+ 
+// If you have other Next.js configurations, you can pass them as the parameter:
+// module.exports = withNextra({ /* other next.js config */ })
---
+++ theme.config.tsx
@@ -0,0 +1,20 @@
+/* eslint sort-keys: error */
+export default {
+  footer: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+        {new Date().getFullYear()} &copy; Toki Pona {' - '} Todos los derechos reservados.
+      <style jsx>{`
+        a {
+          float: right;
+        }
+
+        @media screen and (max-width: 480px) {
+          article {
+            padding-top: 2rem;
+            padding-bottom: 4rem;
+          }
+        }
+      `}</style>
+    </small>
+  )
+}
